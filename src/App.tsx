61cf5adee33ba184d0ad4925dfce927aa4fb4a9d
fluiddevelopmentnet <ruhljeremias@gmail.com>
2024-02-17 21:15:15 +0100

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,39 @@
+import { createSignal } from "solid-js";
+
+import { invoke } from "@tauri-apps/api/tauri";
+
+
+function App() {
+  const [greetMsg, setGreetMsg] = createSignal("");
+  const [name, setName] = createSignal("");
+
+  async function greet() {
+    // Learn more about Tauri commands at https://tauri.app/v1/guides/features/command
+    setGreetMsg(await invoke("greet", { name: name() }));
+  }
+
+  return (
+    <div class="container">
+      
+
+      <form
+        class="row"
+        onSubmit={(e) => {
+          e.preventDefault();
+          greet();
+        }}
+      >
+        <input
+          id="greet-input"
+          onChange={(e) => setName(e.currentTarget.value)}
+          placeholder="Enter a name..."
+        />
+        <button type="submit">Greet</button>
+      </form>
+
+      <p>{greetMsg()}</p>
+    </div>
+  );
+}
+
+export default App;
